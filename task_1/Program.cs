15d6ad442fccb437e968447ca67ae15d4a26570a
Diana Giniyatullina <disshh09022004@gmail.com>
2024-01-27 22:49:07 +0300

    сделали таск1 в лессон 2, написали там прогу, что проверяет, является ли первое число квадратом второго

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//a = 2, b = 10 => нет
+//a = 9, b = -3 => да
+//a = -3, b = 9 => нет
+//Напишите программу, которая на вход принимает два целых числа и проверяет,является ли первое число квадратом второго.
+
+System.Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32 ( Console.ReadLine() );
+System.Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32 ( Console.ReadLine() );
+if ( num1 * num1 == num2 )
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
